22d34adcb9583992112ea381f844156b4cbd307e
emadadel999 <emadadel1234@gmail.com>
2020-08-24 20:20:00 +0200

    hash router

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Welcome from "./pages/Welcome";
 import Game from "./pages/Game";
 import Result from "./pages/Result";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Redirect,
@@ -15,17 +15,12 @@ import './App.css';
 import { Alert } from "reactstrap";
 
 function App() {
-  let history = useHistory();
-  const solvingHandler = () => {
-    history.push('/result');
-  };
-
   return (
     <div className="App">
       <Helmet 
         bodyAttributes={{style: 'background-color : black; min-height: 100%;'}} 
         htmlAttributes={{style: 'height: 100%;'}} />
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" render={() => <Redirect to="/welcome" />} />
           <Route exact path="/welcome">
