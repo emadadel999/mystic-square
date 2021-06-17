207be9328eadfe81a9493a61a02524fec5d51d06
Emad Adel <emadadelabosaif@gmail.com>
2021-06-17 03:40:41 +0200

    fix dependencies

--- src/components/MovesCounter.js
+++ src/components/MovesCounter.js
@@ -1,11 +1,24 @@
 import React from "react";
-import "./MovesCounter.css";
+import styled from "styled-components";
+
+const MoveCounterStyled = styled.div`
+  width: 100%;
+  border: dashed;
+  border-color: #081d45;
+  padding: 10px;
+  text-align: center;
+  p {
+    color: #081d45;
+    font-family: monospace;
+    margin: 0;
+  }
+`;
 
 const MovesCounter = ({ moveCounter }) => {
   return (
-    <div className="numMoves">
+    <MoveCounterStyled className="numMoves">
       <p>Number of moves: {moveCounter}</p>
-    </div>
+    </MoveCounterStyled>
   );
 };
 
--- src/components/ResetBtn.js
+++ src/components/ResetBtn.js
@@ -1,11 +1,40 @@
 import React from "react";
-import "./ResetBtn.css";
+import styled from "styled-components";
+
+const ResetBtnStyled = styled.button`
+  width: 45%;
+  border-radius: 5px;
+  border-color: transparent;
+  text-align: center;
+  cursor: pointer;
+  color: rgb(142, 174, 233);
+  background-color: #081d45;
+  :active {
+    text-decoration: none;
+  }
+  :link {
+    text-decoration: none;
+  }
+  :visited {
+    text-decoration: none;
+  }
+  :hover {
+    text-decoration: none;
+    color: rgb(142, 174, 233);
+    background-color: #05132e;
+  }
+
+  :focus {
+    outline: none;
+    box-shadow: none;
+  }
+`;
 
 const ResetBtn = ({ onPress, children }) => {
   return (
-    <button id="resetBtn" onClick={onPress}>
+    <ResetBtnStyled id="resetBtn" onClick={onPress}>
       {children}
-    </button>
+    </ResetBtnStyled>
   );
 };
 
