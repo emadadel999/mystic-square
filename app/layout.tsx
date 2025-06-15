00541a2e1446c950e451b1d81d42f81eb345138d
Emad Abdelmaksoud <e.abdellmaksoud@gmail.com>
2025-06-15 13:55:36 -0700

    removed logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,39 @@
-import { Metadata } from "next";
-import React from "react";
-import "@/src/styles/main.scss";
+import { Metadata } from 'next';
+import React from 'react';
+import '@/src/styles/main.scss';
 
 export const metadata: Metadata = {
-  title: "Mystic Square",
-  description: "Fun mysterious puzzle game",
+	title: 'Mystic Square',
+	description: 'Fun mysterious puzzle game',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <head>
-        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <meta name="apple-mobile-web-app-title" content="M-Square" />
-      </head>
-      <body>
-        <main className="app-container">
-          {children}
-          <footer className="footer">
+	return (
+		<html lang='en'>
+			<head>
+				<link
+					rel='icon'
+					type='image/png'
+					href='/favicon-96x96.png'
+					sizes='96x96'
+				/>
+				<link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+				<link rel='shortcut icon' href='/favicon.ico' />
+				<link
+					rel='apple-touch-icon'
+					sizes='180x180'
+					href='/apple-touch-icon.png'
+				/>
+				<meta name='apple-mobile-web-app-title' content='M-Square' />
+			</head>
+			<body>
+				<main className='app-container'>
+					{children}
+					{/* <footer className="footer">
               <a
                 className="social-link"
                 href="https://eabdelmaksoud.me"
@@ -37,9 +46,9 @@ export default function RootLayout({
                   width={34}
                 />
               </a>
-          </footer>
-        </main>
-      </body>
-    </html>
-  );
+          </footer> */}
+				</main>
+			</body>
+		</html>
+	);
 }
